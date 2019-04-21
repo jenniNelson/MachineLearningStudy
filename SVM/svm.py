95ee63c94c6a3df7d5be06b73a0b80b1dda9bfc9
jenniNelson <jennimarienelson@gmail.com>
2019-04-20 22:47:01 -0600

    Last commit before starting next assignment

--- SVM/svm.py
+++ SVM/svm.py
@@ -79,7 +79,7 @@ def dual_svm(x, y, C, kernel=np.dot):
     alpha_length = len(x)
     alpha_bounds = Bounds(np.full((alpha_length), 0),np.full((alpha_length), C))
     eq_constraints = {'type': 'eq',
-                      'fun': lambda a: kernel(a, y)
+                      'fun': lambda a: np.dot(a, y)
                       }
 
 
@@ -90,7 +90,7 @@ def dual_svm(x, y, C, kernel=np.dot):
             for j in range(alpha_length):
                 internal_sum += a[j]*y[j] * kernel(x[i], x[j])
             result += internal_sum * a[i] * y[i]
-        return result -sum(a)
+        return result - np.sum(a)
 
 
     init_guess = np.zeros(alpha_length)
@@ -116,14 +116,14 @@ def recover_b_from_alphas(a, w, x, y, threshold):
             num_nonzero += 1
     return b/num_nonzero
 
-def recover_b_from_alphas_only(a, x, y, threshold):
+def recover_b_from_alphas_only(a, x, y, threshold, kernel=np.dot):
     b = 0
     num_nonzero = 0
     for i in range(len(a)):
         if a[i] > threshold:
             inner_sum = 0
             for j in range(len(a)):
-                inner_sum += a[j] * y[j] * np.dot(x[j], x[i])
+                inner_sum += a[j] * y[j] * kernel(x[j], x[i])
             b+= y[i] - inner_sum
             num_nonzero += 1
     return b/num_nonzero
@@ -139,21 +139,23 @@ def kernel_predict(a, x, y, kernel, sample):
     for i in range(len(a)):
         wdotx += a[i]*y[i]*kernel(x[i], sample)
 
-    b = 0
-    num_b = 0
-    for i in range(len(a)):
-        inner_sum = 0
-        for j in range(len(a)):
-            inner_sum += a[j] * y[j] * kernel(x[i], x[j])
-            num_b += 1
-        b += y[i] - inner_sum
+    b = recover_b_from_alphas_only(a, x,y, 1e-11, kernel)
 
-    result = wdotx + b/num_b
+    result = wdotx + b
 
     return -1 if result < 0 else 1
 
 def gaussian_kernel(w, x, gamma):
     return exp(- np.dot(w-x, w-x)/gamma)
+
+
+def count_svm(a):
+    num_svm = 0
+    for alpha in a:
+        if alpha > 0:
+            num_svm += 1
+    return  num_svm
+
 
 
 if __name__ == '__main__':
@@ -213,70 +215,70 @@ if __name__ == '__main__':
 
     format_string = "{:<7.3f}\t&{:>10.3f}\t&{:>10.3f}\t\\\\\hline"
 
-    # for C in C_list:
-    #     test_err = 0
-    #     train_err = 0
-    #     for _ in range(50):
-    #         weights = primal_svm(banknote_train_examples, epochs, learning_rate1(init1, d), C)
-    #         test_err += average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights))
-    #         train_err += average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights))
-    #
-    #     print(format_string.format(C, train_err/50, test_err/50))
-    #
-    # print()
-    #
-    init2 = .4
-    # for C in C_list:
-    #     test_err = 0
-    #     train_err = 0
-    #     for _ in range(50):
-    #         weights = primal_svm(banknote_train_examples, epochs, learning_rate2(init2), C)
-    #         test_err += average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights))
-    #         train_err += average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights))
-    #
-    #     print(format_string.format(C, train_err/50, test_err/50))
+    for C in C_list:
+        test_err = 0
+        train_err = 0
+        for _ in range(50):
+            weights = primal_svm(banknote_train_examples, epochs, learning_rate1(init1, d), C)
+            test_err += average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights))
+            train_err += average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights))
 
-    # 
-    # for C in C_list:
-    #     weights_1 = primal_svm(banknote_train_examples, epochs, learning_rate1(init1, d), C)
-    #     test_err_1 = average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights_1))
-    #     train_err_1 = average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights_1))
-    # 
-    #     weights_2 = primal_svm(banknote_train_examples, epochs, learning_rate2(init2), C)
-    #     test_err_2 = average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights_2))
-    #     train_err_2 = average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights_2))
-    # 
-    #     weights_diff = weights_1-weights_2
-    #     test_err_diff = test_err_1 - test_err_2
-    #     train_err_diff = train_err_1 - train_err_2
-    # 
-    #     _1_string = ""
-    #     _2_string = ""
-    #     _diff_string = ""
-    #     for i in range(len(weights_diff)):
-    #         _1_string += "{:8.3f}\t&".format(weights_1[i])
-    #         _2_string += "{:8.3f}\t&".format(weights_2[i])
-    #         _diff_string += "{:8.3f}\t&".format(weights_diff[i])
-    # 
-    #     total_string = "{:8.3f}\t&".format(C)
-    #     total_string += "LR1 &" + _1_string + "{:8.3f}\t&".format(train_err_1) + "{:8.3f}\t".format(test_err_1)
-    #     total_string += "\n" + r"\\\cline{2-9}&"
-    #     total_string += "LR2 &" +_2_string + "{:8.3f}\t&".format(train_err_2) + "{:8.3f}\t".format(test_err_2)
-    #     total_string += "\n" + r"\\\cline{2-9}&"
-    #     total_string += "DIFF &" +_diff_string + "{:8.3f}\t&".format(train_err_diff) + "{:8.3f}\t".format(test_err_diff)
-    #     total_string += r"\\\hline\hline"
-    # 
-    # 
-    #     print(total_string)
-    # 
-    # 
+        print(format_string.format(C, train_err/50, test_err/50))
+
+    print()
+
+    init2 = .4
+    for C in C_list:
+        test_err = 0
+        train_err = 0
+        for _ in range(50):
+            weights = primal_svm(banknote_train_examples, epochs, learning_rate2(init2), C)
+            test_err += average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights))
+            train_err += average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights))
+
+        print(format_string.format(C, train_err/50, test_err/50))
+
+
+    for C in C_list:
+        weights_1 = primal_svm(banknote_train_examples, epochs, learning_rate1(init1, d), C)
+        test_err_1 = average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights_1))
+        train_err_1 = average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights_1))
+
+        weights_2 = primal_svm(banknote_train_examples, epochs, learning_rate2(init2), C)
+        test_err_2 = average_error(banknote_test_examples, lambda sample: predict_standard(sample, weights_2))
+        train_err_2 = average_error(banknote_train_examples, lambda sample: predict_standard(sample, weights_2))
+
+        weights_diff = weights_1-weights_2
+        test_err_diff = test_err_1 - test_err_2
+        train_err_diff = train_err_1 - train_err_2
+
+        _1_string = ""
+        _2_string = ""
+        _diff_string = ""
+        for i in range(len(weights_diff)):
+            _1_string += "{:8.3f}\t&".format(weights_1[i])
+            _2_string += "{:8.3f}\t&".format(weights_2[i])
+            _diff_string += "{:8.3f}\t&".format(weights_diff[i])
+
+        total_string = "{:8.3f}\t&".format(C)
+        total_string += "LR1 &" + _1_string + "{:8.3f}\t&".format(train_err_1) + "{:8.3f}\t".format(test_err_1)
+        total_string += "\n" + r"\\\cline{2-9}&"
+        total_string += "LR2 &" +_2_string + "{:8.3f}\t&".format(train_err_2) + "{:8.3f}\t".format(test_err_2)
+        total_string += "\n" + r"\\\cline{2-9}&"
+        total_string += "DIFF &" +_diff_string + "{:8.3f}\t&".format(train_err_diff) + "{:8.3f}\t".format(test_err_diff)
+        total_string += r"\\\hline\hline"
+
+
+        print(total_string)
+
+
 
     train_labels = np.array(list(map(lambda sample: sample.label, banknote_train_examples)))
     train_vals = np.array(list(map(lambda sample: sample.attributes[:-1], banknote_train_examples)))
 
     # print(train_vals[:10])
 
-    max_num_samples = 100
+    max_num_samples = 50
     print("TRAINING SUBSET SIZE: ", max_num_samples)
 
     for C in [100/873, 500/873, 700/873]:
@@ -284,17 +286,17 @@ if __name__ == '__main__':
         # print(alphas)
         incorporated_weights = recover_weights(alphas, train_vals[:max_num_samples], train_labels[:max_num_samples], 1e-11)
 
-        train_err = average_error(banknote_train_examples[:max_num_samples],
+        train_err = average_error(banknote_train_examples,
                                   lambda sample: predict_standard(sample, incorporated_weights))
-        test_err = average_error(banknote_train_examples[:max_num_samples],
+        test_err = average_error(banknote_train_examples,
                                  lambda sample: predict_standard(sample, incorporated_weights))
-        other_train_err = average_error(banknote_train_examples[:max_num_samples],
+        other_train_err = average_error(banknote_train_examples,
                                   lambda sample: kernel_predict(alphas, train_vals[:max_num_samples], train_labels[:max_num_samples], np.dot, sample.attributes[:-1]))
-        other_test_err = average_error(banknote_train_examples[:max_num_samples],
+        other_test_err = average_error(banknote_train_examples,
                              lambda sample: kernel_predict(alphas, train_vals[:max_num_samples], train_labels[:max_num_samples], np.dot, sample.attributes[:-1]))
 
-        print(test_err, " ", other_test_err)
-        print("DIFF: ", train_err - other_train_err, ", ", test_err - other_test_err)
+        # print(test_err, " ", other_test_err)
+        # print("DIFF: ", train_err - other_train_err, ", ", test_err - other_test_err)
 
         string = "{:8.3f}".format(C)
         for i in range(len(incorporated_weights)):
@@ -314,9 +316,9 @@ if __name__ == '__main__':
 
     # KERNELSTUFF
 
-    format_string = "{:<7.3f}\t&{:<7.3f}\t&{:>10.3f}\t&{:>10.3f}\t\\\\\hline"
+    format_string = "{:<7.3f}\t&{:<7.3f}\t&{:>10.3f}\t&{:>10.3f}\t&{:>10.0f}\t\\\\\hline"
 
-    for gamma in [10, 100]:
+    for gamma in [0.01, 0.1, 0.5, 1, 2, 5, 10, 100]:
        for C in [100/873, 500/873, 700/873]:
             alphas = dual_svm(train_vals[:max_num_samples], train_labels[:max_num_samples], C, lambda w,x : gaussian_kernel(w,x, gamma))
             # print(alphas)
@@ -326,11 +328,28 @@ if __name__ == '__main__':
             # train_err = average_error(banknote_train_examples[:max_num_samples], lambda sample : kernel_predict(alphas, train_vals[:max_num_samples], train_labels[:max_num_samples], lambda w,x:gaussian_kernel(w, x, gamma), sample.attributes[:-1]))
             # test_err = average_error(banknote_test_examples[:max_num_samples], lambda sample : kernel_predict(alphas, train_vals[:max_num_samples], train_labels[:max_num_samples], lambda w,x:gaussian_kernel(w, x, gamma), sample.attributes[:-1]))
 
-            print(format_string.format(C, gamma, train_err, test_err))
+            print(format_string.format(C, gamma, train_err, test_err, count_svm(alphas)))
+
+
+    old_sv = list()
+    for gamma in [0.01, 0.1, 0.5, 1, 2, 5, 10, 100]:
+        C = 500/873
+        alphas = dual_svm(train_vals[:max_num_samples], train_labels[:max_num_samples], C,
+                          lambda w, x: gaussian_kernel(w, x, gamma))
+        new_sv = list()
+        num_overlap = 0
+        for i in range(len(alphas)):
+            if alphas[i] > 0:
+                new_sv.append(i)
+        for sv in old_sv:
+            if sv in new_sv:
+                num_overlap += 1
+
+        print("{:<7.3f}\t&{:>10.0f}\t\\\\\hline".format(gamma, num_overlap))
+        old_sv = new_sv
 
 
 
 
 
 
-    
